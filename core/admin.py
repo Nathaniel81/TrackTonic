df83fdbfa88b464388e717bae5ebc83081d953f6
Nathaniel81 <nathanield356@gmail.com>
2023-10-14 18:22:46 +0300

    First commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, PlayList, Song
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(PlayList)
+admin.site.register(Song)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+def user_img(instance, filename):
+    return f'user_{instance.id}/{filename}'
+
+def user_dir(instance, filename):
+    return f'user_{instance.owner.id}/{filename}'
+
+def user_dir_song(instance, filename):
+    return f'user_{instance.playlist.owner.id}/{filename}'
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True, null=True)
+    avatar = models.ImageField(upload_to=user_img, null=True, blank=True, default='avatar.svg')
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.name
+
+class PlayList(models.Model):
+    playlist_name = models.CharField(max_length=40)
+    owner = models.ForeignKey(User, related_name='playlists', on_delete=models.CASCADE)
+    genre = models.CharField(max_length=200, null=False, blank=False)
+    cover = models.ImageField(upload_to=user_dir, blank=True, null=True)
+    description = models.TextField(max_length=300, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Playlists'
+
+    def __str__(self):
+        return self.playlist_name
+
+class Song(models.Model):
+    song_name = models.CharField(max_length=40)
+    playlist = models.ForeignKey(PlayList, related_name='songs', on_delete=models.CASCADE)
+    music_file = models.FileField(upload_to=user_dir_song)
+
+    def __str__(self):
+        return self.song_name
