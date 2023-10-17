08666e7d19bb34eb888e3f75244f055fc0872311
Nathaniel81 <nathanield356@gmail.com>
2023-10-17 22:07:14 +0300

    Add pre-delete signals to remove associated files upon deletion

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
+import os
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -10,7 +13,6 @@ def user_dir(instance, filename):
 
 def user_dir_song(instance, filename):
     return f'user_{instance.playlist.owner.id}/{filename}'
-
 
 class User(AbstractUser):
     name = models.CharField(max_length=40, unique=True)
@@ -44,3 +46,23 @@ class Song(models.Model):
 
     def __str__(self):
         return self.song_name
+
+@receiver(pre_delete, sender=Song)
+def song_delete(sender, instance, **kwargs):
+    if instance.music_file:
+        print("Song File Path:", instance.music_file.path)
+        if os.path.isfile(instance.music_file.path):
+            print("File Exists. Removing...")
+            os.remove(instance.music_file.path)
+        else:
+            print("File does not exist")
+
+@receiver(pre_delete, sender=PlayList)
+def playlist_delete(sender, instance, **kwargs):
+    if instance.cover:
+        print("Playlist Cover Path:", instance.cover.path)
+        if os.path.isfile(instance.cover.path):
+            print("File Exists. Removing...")
+            os.remove(instance.cover.path)
+        else:
+            print("File does not exist")
