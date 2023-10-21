a8ee118a6615f13658278dfea5f0fccd374dbb41
Nathaniel81 <nathanield356@gmail.com>
2023-10-21 12:30:25 +0300

    Refactored models with abstract classes

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import User, PlayList, Song
+from .models import User, PlayList, Song, Album, AlbumLike, PlayListLike, SongLike
 
 admin.site.register(User)
 admin.site.register(PlayList)
+admin.site.register(PlayListLike)
 admin.site.register(Song)
+admin.site.register(Album)
+admin.site.register(AlbumLike)
+admin.site.register(SongLike)
 
