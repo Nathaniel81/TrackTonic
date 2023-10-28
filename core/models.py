cd4168ece749346d5e8840160be4f18369da0c0d
Nathaniel81 <nathanield356@gmail.com>
2023-10-28 12:12:56 +0300

    Updated the MusicFileValidator class to correctly validate music file extensions during the file upload process.

--- core/models.py
+++ core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 
 
 def user_img(instance, filename):
@@ -21,6 +23,17 @@ def get_upload_path(instance, filename):
         return user_dir_playlist_song(instance, filename)
     else:
         return user_dir_album_song(instance, filename)
+
+@deconstructible
+class MusicFileValidator:
+    def __call__(self, value):
+        valid_extensions = ['mp3', 'm4a','wav', 'ogg', 'flac']
+        ext = value.name.split('.')[-1]
+        print(f"File extension: {ext}")
+        print(f"Valid extensions: {valid_extensions}")
+        if not ext.lower() in valid_extensions:
+            raise ValidationError('Unsupported file extension.')
+
 
 class User(AbstractUser):
     name = models.CharField(max_length=40, unique=True)
@@ -89,14 +102,11 @@ class AlbumLike(Like):
 
 class Song(models.Model):
     song_name = models.CharField(max_length=40)
-    # playlist = models.ForeignKey(PlayList, related_name='songs', on_delete=models.CASCADE)
-    # album = models.ForeignKey(Album, related_name='songs', on_delete=models.CASCADE)
-    
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-    
-    music_file = models.FileField(upload_to=get_upload_path)
+    music_file = models.FileField(upload_to=get_upload_path, validators=[MusicFileValidator()])
+    # music_file = models.FileField(upload_to=get_upload_path)
     genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
